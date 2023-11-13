32ffddc777c5c28419def7f2ca168d20f34e0679
Hutch Long <24longj@depaulcatholic.org>
2023-11-13 10:24:42 -0500

    adding initial controller and model implementation

---
+++ content-calendar/src/main/java/edu/depaul/contentcalendar/repository/ContentCollectionRepository.java
@@ -0,0 +1,44 @@
+package edu.depaul.contentcalendar.repository;
+
+import edu.depaul.contentcalendar.model.Content;
+import edu.depaul.contentcalendar.model.Status;
+import edu.depaul.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ContentCollectionRepository {
+
+    private final List<Content> content = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+    }
+
+    public List<Content> findAll() {
+        return content;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(1,
+                "My First Blog Post",
+                "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        content.add(c);
+    }
+
+}
